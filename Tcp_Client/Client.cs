49fe4baa3dcb092afc9498a66a0450731266f05b
NewFolder <nahim13@gmail.com>
2018-06-14 23:07:08 +0300

    Добавьте файлы проекта.

---
+++ Tcp_Client/Client.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Tcp_Client
+{
+    class Client
+    {
+        class TestSocket
+        {
+            Socket socketCln = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            byte[] buffer = new byte[1024];
+
+            ~TestSocket()
+            {
+                socketCln.Shutdown(SocketShutdown.Both);
+                socketCln.Close();
+            }
+
+            public void StartSocket()
+            {
+                socketCln.Connect(IPAddress.Parse("192.168.1.149"), 50);
+
+                while (true)
+                {
+                    string message = Console.ReadLine();
+                    buffer = Encoding.UTF8.GetBytes(message);
+
+                    socketCln.Send(buffer);
+                    buffer = new byte[1024];
+                    int bytes = socketCln.Receive(buffer);
+                    Console.WriteLine((Encoding.UTF8.GetString(buffer, 0, bytes)));
+                }
+            }
+        }
+
+        class TestTcpClient
+        {
+            TcpClient tcpClient = new TcpClient();
+            NetworkStream stream;
+
+            ~TestTcpClient()
+            {
+                stream.Close();
+                tcpClient.Close();
+            }
+
+            public void StartTcpclient()
+            {
+                tcpClient.Connect(IPAddress.Parse("192.168.1.149"), 50);
+                stream = tcpClient.GetStream();
+
+                Task task = Task.Run(() =>
+                 {
+                     byte[] buffer;
+                     string message;
+                     Thread.CurrentThread.IsBackground = false;
+
+                     while (true)
+                     {
+                         message = Console.ReadLine();
+
+                         buffer = Encoding.UTF8.GetBytes(message);
+                         stream.Write(buffer, 0, buffer.Length);
+                     }
+                 });
+
+
+                Task.Run(() =>
+                {
+                    byte[] buffer;
+                    string message;
+                    Thread.CurrentThread.IsBackground = false;
+
+                    while (true)
+                    {
+                        buffer = new byte[1024];
+                        int bytes = stream.Read(buffer, 0, buffer.Length);
+
+                        message = Encoding.UTF8.GetString(buffer, 0, bytes);
+                        Console.WriteLine(message);
+                    }
+                });
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            new TestSocket().StartSocket();
+            //new TestTcpClient().StartTcpclient();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Tcp_Server/Server.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text.RegularExpressions;
+using System.Net;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tcp_Server
+{
+    class Server
+    {
+        class TestSocket
+        {
+             Socket socketSrv = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            
+
+            public void StartSocket()
+            {
+                socketSrv.Bind(new IPEndPoint(IPAddress.Any, 50));
+                socketSrv.Listen(5);
+
+                while (true)
+                {
+                    Socket socketClient = socketSrv.Accept(); // Блокирует вызывающий поток до появления нового подключения
+
+                    Console.WriteLine("New conection");
+
+                    Task.Run(() =>
+                   {
+                       for (; ; )
+                       {
+                           byte[] buffer = new byte[1024];
+
+                           int bytes = socketClient.Receive(buffer);
+                           Console.WriteLine((Encoding.UTF8.GetString(buffer, 0, bytes)));
+                           buffer = Encoding.ASCII.GetBytes("Message received");
+                           socketClient.Send(buffer);
+                       }
+                   });
+                }
+
+                socketSrv.Shutdown(SocketShutdown.Both);
+                socketSrv.Close();
+            }
+        }
+
+        class TestTcpListener
+        {
+            static TcpListener tcpListener = new TcpListener(new IPEndPoint(IPAddress.Any, 50));
+            NetworkStream stream;
+
+            public void StartTcpListener()
+            {
+                tcpListener.Start();
+
+                while (true)
+                {
+                    TcpClient tcpClient = tcpListener.AcceptTcpClient(); //Socket socket = tcpListener.AcceptSocket();
+
+                    Console.WriteLine("New conection");
+
+                    Task.Run(() =>
+                    {
+                        stream = tcpClient.GetStream();
+                        for (; ; )
+                        {
+                            byte[] buffer = new byte[1024];
+
+                            int bytes = stream.Read(buffer, 0, buffer.Length);
+                            Console.WriteLine((Encoding.UTF8.GetString(buffer, 0, bytes)));
+
+                            buffer = Encoding.ASCII.GetBytes("Message received");
+                            stream.Write(buffer, 0, buffer.Length);
+                        }
+                        stream.Close();
+                        tcpClient.Close();
+                    });
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+         //new TestSocket().StartSocket();
+           new TestTcpListener().StartTcpListener();
+
+        }
+    }
+}
